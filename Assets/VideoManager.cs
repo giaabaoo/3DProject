f4651d76e8a70299ba573334b162d5a1d889031e
giaabaoo <dhgbao@apcs.vn>
2020-09-27 23:09:28 +0700

    final

---
+++ Assets/VideoManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+public class VideoManager : MonoBehaviour
+{
+   public VideoPlayer vid;
+
+   public void Play() {
+       vid.Play();
+   }
+
+   public void Stop() {
+       vid.Stop();
+   }
+}
